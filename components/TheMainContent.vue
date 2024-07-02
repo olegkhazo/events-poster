a498fd87a5c187a7f554e903f6473d3b94c82a13
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-07-02 16:55:40 +0200

    [WIP] create components folder, create footer, header, mainContent components

---
+++ components/TheMainContent.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>Main Content</h1>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/_variables.scss";
+
+main {
+  width: 68%;
+  background-color: $gray-300;
+
+  .mains-header {
+    background-color: $gray-800;
+    padding-left: 15px;
+
+    h1 {
+      color: $white;
+    }
+  }
+}
+</style>
