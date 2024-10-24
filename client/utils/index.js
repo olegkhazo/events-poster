2a1a787f1d55adc0695175251a18cddc8263572a
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-10-24 15:13:48 +0200

    delete data updaiting function

--- client/utils/index.js
+++ client/utils/index.js
@@ -1,30 +1,5 @@
 import { FORM_VALIDATION_PATTERNS } from "@/utils/constants";
 
-export function updateFormatOfEventDate(eventData) {
-  const splitDate = eventData.split(" ");
-  const datePart = splitDate.slice(-1)[0];
-
-  // Check the string format for DD/MM/YYYY
-  const slashDateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
-
-  // Check the string format for DD.MM (без года)
-  const dotDateRegexWithoutYear = /^\d{2}\.\d{2}$/;
-
-  // Check the string format for DD.MM.YYYY
-  const dotDateRegex = /^\d{2}\.\d{2}\.\d{4}$/;
-
-  if (slashDateRegex.test(datePart)) {
-    return datePart;
-  } else if (dotDateRegex.test(datePart)) {
-    // Convert DD.MM.YYYY to DD/MM/YYYY
-    return datePart.replace(/\./g, "/");
-  } else if (dotDateRegexWithoutYear.test(datePart)) {
-    return `${datePart}.2024`.replace(/\./g, "/");
-  } else {
-    return "";
-  }
-}
-
 export function sortByDate(events) {
   return events
     .filter(event => event.eventDate) 
