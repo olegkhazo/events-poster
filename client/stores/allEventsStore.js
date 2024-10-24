0878423aa2d9abbaa760e941b2df266ab89aa32c
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-10-24 15:12:31 +0200

    remove redundant sorting for events in eventsStore

--- client/stores/allEventsStore.js
+++ client/stores/allEventsStore.js
@@ -1,58 +1,19 @@
-import { sortByDate } from "~/utils";
 
 export const useAllEventsStore = defineStore('all-events-store', () => {
   const allEvents = ref([]);
   const sortedByDateEventsCollection = ref([]);
   const currentFilteredEventCollection = ref([]);
 
+  
+  
   watch(allEvents, (newVal) => {
-    if (newVal.length > 0) {
-      const uniqueEventsMap = new Map();
+    const eventsArray = newVal.value || newVal;
 
-      newVal.forEach(event => {
-        const normalizeString = (str) => {
-          return str ? str.toLowerCase().replace(/\s+/g, '').replace(/[^\w\s]/gi, '') : '';
-        };
+    console.log('Events length:', eventsArray.length); 
 
-        const { eventDate, eventTime, eventTitle } = event;
 
-        let shouldAddEvent = true;
-
-        uniqueEventsMap.forEach((existingEvent, key) => {
-          const { eventDate: existingDate, eventTime: existingTime, eventTitle: existingTitle } = existingEvent;
-
-          const normalizedExistingDate = normalizeString(existingDate);
-          const normalizedEventDate = normalizeString(eventDate);
-
-          const normalizedExistingTime = normalizeString(existingTime);
-          const normalizedEventTime = normalizeString(eventTime);
-
-          const normalizedExistingTitle = normalizeString(existingTitle);
-          const normalizedEventTitle = normalizeString(eventTitle);
-
-          const dateMatch = (normalizedExistingDate.includes(normalizedEventDate) || normalizedEventDate.includes(normalizedExistingDate));
-          const timeMatch = (normalizedExistingTime.includes(normalizedEventTime) || normalizedEventTime.includes(normalizedExistingTime));
-          const titleMatch = (normalizedExistingTitle.includes(normalizedEventTitle) || normalizedEventTitle.includes(normalizedExistingTitle));
-
-          if (dateMatch && timeMatch && titleMatch) {
-            shouldAddEvent = false;
-
-            // Если новое событие имеет более полное название, заменить существующее
-            if (eventTitle && eventTitle.length > (existingTitle?.length || 0)) {
-              uniqueEventsMap.set(key, event);
-            }
-          }
-        });
-
-        if (shouldAddEvent) {
-          const key = `${normalizeString(eventDate)}-${normalizeString(eventTime)}-${normalizeString(eventTitle)}`;
-          uniqueEventsMap.set(key, event);
-        }
-      });
-
-      const uniqueEvents = Array.from(uniqueEventsMap.values());
-
-      sortedByDateEventsCollection.value = sortByDate(uniqueEvents);
+    if (eventsArray.length > 0) {
+      sortedByDateEventsCollection.value = newVal;
     }
   });
 
