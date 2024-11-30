764640e4841962e425ac7da78eeaef141763d983
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-11-30 22:06:14 +0100

    fix subscribe controller

--- server/src/controllers/subscribeController.ts
+++ server/src/controllers/subscribeController.ts
@@ -3,44 +3,44 @@ import { SubscribeModel } from '../models/subscribe.models';
 
 
 export const getAllSubscriptions = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const allSubscriptions = await SubscribeModel.find().sort({ event_date: 1 });
-  
-      res.status(200).json(allSubscriptions);
-    } catch (error) {
-      next(error);
-    }
-  };
+  try {
+    const allSubscriptions = await SubscribeModel.find().sort({ event_date: 1 });
+
+    res.status(200).json(allSubscriptions);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const createSubscription = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const newSubscriptionData: any = { ...req.body };  
-     
-      const newSubscription = new SubscribeModel(newSubscriptionData);
-      await newSubscription.save();
-      
-      res.status(201).json(newSubscription);
-    } catch (error) {
-      console.error("Error creating subscription:", error);
-      
-      if (error instanceof Error) {
-        res.status(500).json({ error: "Internal Server Error", details: error.message });
-      } else {
-        res.status(500).json({ error: "Internal Server Error", details: "Unknown error" });
-      }
+  try {
+    const newSubscriptionData: any = { ...req.body };  
+   
+    const newSubscription = new SubscribeModel(newSubscriptionData);
+    await newSubscription.save();
+    
+    res.status(201).json(newSubscription);
+  } catch (error) {
+    console.error("Error creating subscription:", error);
+    
+    if (error instanceof Error) {
+      res.status(500).json({ error: "Internal Server Error", details: error.message });
+    } else {
+      res.status(500).json({ error: "Internal Server Error", details: "Unknown error" });
     }
-  };
+  }
+};
 
 
 export const deleteSubscription = async (req: Request, res: Response, next: NextFunction) => {
-const subscriptionId = req.params.id;
+  const subscriptionId = req.params.id;
 
-try {
-  const deletedSubscription = await SubscribeModel.findByIdAndDelete(subscriptionId);
+  try {
+    const deletedSubscription = await SubscribeModel.findByIdAndDelete(subscriptionId);
 
-  if (!deletedSubscription) {
-    return res.status(404).json({ message: 'Subscription not found' });
-  }
+    if (!deletedSubscription) {
+      return res.status(404).json({ message: 'Subscription not found' });
+    }
 
   res.status(200).json({ message: 'Subscription deleted successfully' });
     } catch (error) {
