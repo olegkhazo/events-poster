74b2e14441bd858af20a3483376204227facc630
Oleg Zhuravko <o.zhuravko@gmail.com>
2025-03-03 15:06:15 +0100

    event update controller

--- server/src/controllers/eventsController.ts
+++ server/src/controllers/eventsController.ts
@@ -144,13 +144,10 @@ export const createEvent = async (req: Request, res: Response, next: NextFunctio
   try {
     const newEventData: any = { ...req.body };
 
-    // Проверка на наличие изображения в base64
     if (newEventData.event_image_blob) {
-      // Здесь изображение уже в base64, так что дополнительной обработки не требуется
       console.log("Base64 image received:", newEventData.event_image_blob);
     }
 
-    // Создаем и сохраняем событие
     const newEvent = new EventModel(newEventData);
     await newEvent.save();
 
@@ -166,6 +163,22 @@ export const createEvent = async (req: Request, res: Response, next: NextFunctio
   }
 };
 
+export const eventUpdate = async (req: Request, res: Response, next: NextFunction) => {
+  const eventId = req.params.id;
+  const updateData = req.body;
+
+  try {
+    const updatedEvent = await EventModel.findByIdAndUpdate(eventId, updateData, { new: true });
+
+    if (!updatedEvent) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+
+    res.status(200).json({ message: "Event updated successfully", event: updatedEvent });
+  } catch (error) {
+    next(error);
+  }
+};
 
 
 export const deleteEvent = async (req: Request, res: Response, next: NextFunction) => {
