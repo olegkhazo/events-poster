ecb4f7d52f89873aa165eaa99be9aeb468569596
Oleg Zhuravko <o.zhuravko@gmail.com>
2025-03-13 10:36:14 +0100

    phone

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -7,7 +7,7 @@ export const FORM_VALIDATION_PATTERNS = {
   COMMON_NOT_EMPTY_PATTERN: /^\s*[a-zA-Z0-9-]+\s*(?:\s+[a-zA-Z0-9-]+)*\s*$/,
   PASSWORD_PATTERN: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]).{8,}$/,
   EMAIL_PATTERN: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-  PHONE_PATTERN: /^.{1,12}$/,
+  PHONE_PATTERN: /^.{1,16}$/,
   ENGINE_VOLUME_PATTERN: /^(?:\d|1\d|20)(\.\d\d?)?$/,
   PRICE_PATTERN: /^\d{1,6}$/,
   URL_PATTERN: /^(https?:\/\/)?([\w\-]+\.)+[\w\-]+(\/[\w\-.,@?^=%&:/~+#]*)?$/i,
